23f97c8f0e4750cc343fc8cecf413e7b473240d2
Anton Surkis <antonsurkis@gmail.com>
2022-10-26 15:42:56 +0300

    Solve task 2 (unoptimized)

--- task.py
+++ task.py
@@ -87,28 +87,89 @@ class Perceptron:
 
 # Task 2
 
-class PerceptronBest(Perceptron):
-    def __init__(self, iterations: int = 100):
-        super().__init__(iterations)
-    
-    
-    def _fit_step(self, X: np.ndarray, y: np.ndarray):
-        pred = np.sign(X @ self.w.T)
-        neq = y != pred
+class PerceptronBest:
 
-        neq_sum = np.sum(neq)
-        if neq_sum < self.best_neq:
-            self.w_best = np.copy(self.w)
-            self.best_neq = neq_sum
+    def __init__(self, iterations: int = 100):
+        """
+        Parameters
+        ----------
+        iterations : int
+        Количество итераций обучения перцептрона.
+
+        Attributes
+        ----------
+        w : np.ndarray
+        Веса перцептрона размерности X.shape[1] + 1 (X --- данные для обучения), 
+        w[0] должен соответстовать константе, 
+        w[1:] - коэффициентам компонент элемента X.
+
+        Notes
+        -----
+        Вы можете добавлять свои поля в класс.
         
-        self.w[:] += np.sum(y[neq].reshape((-1, 1)) * X[neq, :], axis=0)
+        """
+
+        self.iterations = iterations
+        self.w = None
     
     
-    def _fit(self, X: np.ndarray, y: np.ndarray):
-        self.w_best = None
-        self.best_neq = y.shape[0]
-        super()._fit(X, y)
-        self.w = self.w_best
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        """
+        Обучает перцептрон.
+
+        Для этого сначала инициализирует веса перцептрона, 
+        а затем обновляет их в течении iterations итераций.
+
+        При этом в конце обучения оставляет веса, 
+        при которых значение accuracy было наибольшим.
+        
+        Parameters
+        ----------
+        X : np.ndarray
+            Набор данных, на котором обучается перцептрон.
+        y: np.ndarray
+            Набор меток классов для данных.
+        
+        """
+        yh = 2 * y - 1
+        Xh = np.hstack((np.tile(1, (X.shape[0], 1)), X))
+        self.w = np.zeros(Xh.shape[1])
+        
+        w_best = None
+        neq_best = yh.shape[0] + 1
+        
+        for _ in range(self.iterations):
+            pred = np.sign(Xh @ self.w.T)
+            neq = yh != pred
+            
+            neq_sum = neq.sum()
+            if neq_sum < neq_best:
+                w_best = np.copy(self.w)
+                neq_best = neq_sum
+            
+            self.w[:] += np.sum(yh[neq].reshape((-1, 1)) * Xh[neq, :], axis=0)
+        
+        self.w = w_best
+    
+    
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Предсказывает метки классов.
+        
+        Parameters
+        ----------
+        X : np.ndarray
+            Набор данных, для которого необходимо вернуть метки классов.
+        
+        Return
+        ------
+        labels : np.ndarray
+            Вектор индексов классов 
+            (по одной метке для каждого элемента из X).
+        
+        """
+        real = self.w[0] + X @ self.w[1:].T
+        return (np.sign(real) + 1) // 2
     
 # Task 3
 
